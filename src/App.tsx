aea69abbdff2af1895a7df12ad59b6f78755fab9
Marcelo Macedo <marcelo3macedo@gmail.com>
2021-07-13 15:42:32 -0300

    Service Language

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
 import React from 'react';
-import { setTranslations, setDefaultLanguage } from 'react-multi-lang';
 import { BrowserRouter } from 'react-router-dom';
 import { GlobalStyle } from './styles/global';
-
 import Routes from "./routes";
-import pt from "@assets/lang/pt.json";
-
-setTranslations({pt})
-setDefaultLanguage('pt')
+import "./services/Language";
 
 function App() {
   return (
