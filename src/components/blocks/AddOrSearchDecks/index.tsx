5f481f0a39d04023c47c29c8cf15dba0c8ab9cdd
Marcelo Macedo <marcelo3macedo@gmail.com>
2021-09-11 11:20:21 -0300

    Fix compile

--- src/components/blocks/AddOrSearchDecks/index.tsx
+++ src/components/blocks/AddOrSearchDecks/index.tsx
@@ -1,21 +1,12 @@
 import React from 'react';
-import { useDispatch } from "react-redux";
 import { useTranslation } from 'react-multi-lang';
 
-import ButtonPrimary from '@components/button/ButtonPrimary';
 import SearchDecks from '@components/blocks/SearchDecks';
-import { navigatePush } from '@store/modules/navigate/actions';
-import { PATH_ADDDECK } from '@services/Navigation';
 
-import { Wrapper, Content, Title, AddArea } from './styles';
+import { Wrapper, Content } from './styles';
 
 export default function AddOrSearchDecks() {
-  const dispatch = useDispatch();
   const t = useTranslation();
-
-  function addClick() {
-    dispatch(navigatePush({ path: PATH_ADDDECK }));
-  }
 
   return (
     <Wrapper>
@@ -24,10 +15,4 @@ export default function AddOrSearchDecks() {
       </Content>
     </Wrapper>
   );
-
-  /*<Title>{t('decks.add')}</Title>
-        <AddArea>
-          <ButtonPrimary content={t('actions.add')} action={addClick}/>
-        </AddArea>
-        */
 }
