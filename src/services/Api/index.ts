529c8854c0d7cbab8cbbc1841b3cd87147798700
Marcelo Macedo <marcelo3macedo@gmail.com>
2021-09-16 08:25:21 -0300

    Fix baseUrl

--- src/services/Api/index.ts
+++ src/services/Api/index.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { LS_TOKEN } from "@services/LocalStorage";
 
 const api = axios.create({
-    baseURL: process.env.API_URL,
+    baseURL: process.env.REACT_APP_API_URL,
     withCredentials: true,
     headers: {
         Authorization: `Bearer ${localStorage.getItem(LS_TOKEN)}`,
