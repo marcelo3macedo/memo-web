bb370577de294fffa0f54e8d6e75b72bd68fa1a1
Marcelo Alberico Macedo <marcelo3macedo@gmail.com>
2021-11-09 09:01:38 -0300

    Adding session info

--- src/pages/main/Session/index.tsx
+++ src/pages/main/Session/index.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import { useTranslation } from 'react-multi-lang';
 
-import CardsTotal from '@components/blocks/CardsTotal';
-
-import { Wrapper, Content, Title, Header, Action } from './styles';
+import { Wrapper, Content, Title, Header, SubTitle, Action, Themes, ThemeTitle, Card, CardName } from './styles';
 import ButtonPrimary from '@components/button/ButtonPrimary';
 import { RootState } from '@store/modules/rootReducer';
 import { reviewAction } from '@store/modules/session/actions';
@@ -26,12 +24,27 @@ export default function Session() {
     <Wrapper>
       <Content>
         <Header>
+          <SubTitle>{t('decks.title')}</SubTitle>
           <Title>{session.deck.name}</Title>
-          <CardsTotal size={session.cards ? session.cards.length : 0}/>
         </Header>
         <Action>
           <ButtonPrimary content={t('actions.review')} action={reviewClick}/>
         </Action>
+
+        <Themes>
+          <ThemeTitle>{t('decks.themeTitle')}</ThemeTitle>
+          {session.cards ? 
+            session.cards.map(i => i.title)
+              .filter((value, index, self) => {
+                return self.indexOf(value) == index;
+              }).map(d => (
+                <Card key={d}>
+                  <CardName>{d}</CardName>
+                </Card>
+              ))
+            : <></>
+          }
+        </Themes>
       </Content>
     </Wrapper>
   ); 
