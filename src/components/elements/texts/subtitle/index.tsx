d38706d56a6b48d47f17b9bcad247644b4095f9f
Marcelo Macedo <marcelo3macedo@gmail.com>
2023-10-16 10:31:23 -0300

    fix: lines

--- src/components/elements/texts/subtitle/index.tsx
+++ src/components/elements/texts/subtitle/index.tsx
@@ -3,7 +3,7 @@ import { Styles, TextProps } from '@interfaces/texts/TextProps';
 import { Text } from './styles';
 
 export function SubtitleText({ value, styled = Styles.Default }: TextProps) {
-  const lines = value.split('|');
+  const lines = value.toString().split('|');
 
   return lines.map((l, i) => (
     <Text key={i} styleName={styled}>
